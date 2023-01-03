b3ece034d9fadcfd6616880a723fbb16d379f1d8
Jan-Niklas Burfeind <git@aiyionpri.me>
2023-01-03 15:59:11 +0100

    gluon-mesh-batman-adv: fix c indentation

--- package/gluon-mesh-batman-adv/src/respondd-statistics.c
+++ package/gluon-mesh-batman-adv/src/respondd-statistics.c
@@ -79,7 +79,7 @@ static int parse_gw_list_netlink_cb(struct nl_msg *msg, void *arg)
 	char addr[18];
 
 	opts = batadv_container_of(query_opts, struct gw_netlink_opts,
-				   query_opts);
+			query_opts);
 
 	if (!genlmsg_valid_hdr(nlh, 0))
 		return NL_OK;
@@ -90,11 +90,11 @@ static int parse_gw_list_netlink_cb(struct nl_msg *msg, void *arg)
 		return NL_OK;
 
 	if (nla_parse(attrs, BATADV_ATTR_MAX, genlmsg_attrdata(ghdr, 0),
-		      genlmsg_len(ghdr), batadv_genl_policy))
+				genlmsg_len(ghdr), batadv_genl_policy))
 		return NL_OK;
 
 	if (batadv_genl_missing_attrs(attrs, gateways_mandatory,
-				      BATADV_ARRAY_SIZE(gateways_mandatory)))
+				BATADV_ARRAY_SIZE(gateways_mandatory)))
 		return NL_OK;
 
 	if (!attrs[BATADV_ATTR_FLAG_BEST])
@@ -127,8 +127,8 @@ static void add_gateway(struct json_object *obj) {
 	};
 
 	batadv_genl_query("bat0", BATADV_CMD_GET_GATEWAYS,
-			  parse_gw_list_netlink_cb, NLM_F_DUMP,
-			  &opts.query_opts);
+			parse_gw_list_netlink_cb, NLM_F_DUMP,
+			&opts.query_opts);
 }
 
 static inline bool ethtool_ioctl(int fd, struct ifreq *ifr, void *data) {
@@ -236,7 +236,7 @@ static struct json_object * get_traffic(void) {
 	json_object_object_add(ret, "mgmt_rx", mgmt_rx);
 	json_object_object_add(ret, "mgmt_tx", mgmt_tx);
 
- out:
+out:
 	free(stats);
 	free(strings);
 	close(fd);
@@ -263,7 +263,7 @@ static int parse_clients_list_netlink_cb(struct nl_msg *msg, void *arg)
 	uint32_t flags, lastseen;
 
 	opts = batadv_container_of(query_opts, struct clients_netlink_opts,
-				   query_opts);
+			query_opts);
 
 	if (!genlmsg_valid_hdr(nlh, 0))
 		return NL_OK;
@@ -274,11 +274,11 @@ static int parse_clients_list_netlink_cb(struct nl_msg *msg, void *arg)
 		return NL_OK;
 
 	if (nla_parse(attrs, BATADV_ATTR_MAX, genlmsg_attrdata(ghdr, 0),
-		      genlmsg_len(ghdr), batadv_genl_policy))
+				genlmsg_len(ghdr), batadv_genl_policy))
 		return NL_OK;
 
 	if (batadv_genl_missing_attrs(attrs, clients_mandatory,
-				      BATADV_ARRAY_SIZE(clients_mandatory)))
+				BATADV_ARRAY_SIZE(clients_mandatory)))
 		return NL_OK;
 
 	flags = nla_get_u32(attrs[BATADV_ATTR_TT_FLAGS]);
@@ -304,8 +304,8 @@ static struct json_object * get_clients(void) {
 	};
 
 	batadv_genl_query("bat0", BATADV_CMD_GET_TRANSTABLE_LOCAL,
-			  parse_clients_list_netlink_cb, NLM_F_DUMP,
-			  &opts.query_opts);
+			parse_clients_list_netlink_cb, NLM_F_DUMP,
+			&opts.query_opts);
 
 	struct json_object *ret = json_object_new_object();
 
